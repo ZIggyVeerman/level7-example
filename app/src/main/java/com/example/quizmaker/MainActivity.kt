8fae5346f398e0a8a1a6a2d9af5e92d7f95dc783
Ziggy Veerman <ziggy.veerman@hva.nl>
2020-06-12 15:31:01 +0200

    example done

--- app/src/main/java/com/example/quizmaker/MainActivity.kt
+++ app/src/main/java/com/example/quizmaker/MainActivity.kt
@@ -1,39 +1,42 @@
 package com.example.quizmaker
 
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
+import android.os.Bundle
 import android.view.MenuItem
-
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: QuizViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        viewModel = ViewModelProvider(this).get(QuizViewModel::class.java)
+        initNavigation()
+    }
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+    private fun initNavigation() {
+        // The NavController
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        // Connect the navHostFragment with the ActionBar.
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                navController.navigateUp()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item!!)
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
